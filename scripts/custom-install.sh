5ef8665381eedd4308dd3a1bf36639c960a0fc16
Michael Bonetti <michael.bonetti@incyclesoftware.com>
2022-04-16 14:06:56 -0400

    Clean up install scrips

--- scripts/custom-install.sh
+++ scripts/custom-install.sh
@@ -34,25 +34,13 @@ helm --namespace monitoring upgrade --install promtail grafana/promtail --versio
 
 echo "Installing MultiJuicer..."
 helm repo add multi-juicer https://iteratec.github.io/multi-juicer/
-helm install multi-juicer multi-juicer/multi-juicer \
-  --set balancer.metrics.enabled=true \
-  --set balancer.metrics.dashboards.enabled=true \
-  --set balancer.metrics.serviceMonitor.enabled=true \
-  --set balancer.metrics.basicAuth.password="GNVUfA2WyhAVGDXZbjCU" \
-  --set grafana.adminPassword="N!K&brr&i4dsn*v8*Jn" \
-  --set juiceShop.maxInstances=20 \
-  # --set juiceShop.config= \
-  # --set ingress.enabled=true \
-  # --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx \
-  # --set ingress.hosts[0].host="cybersecurity.tsa-challange.org" \
-  # --set ingress.hosts[0].paths[0]="/"
-  
+helm install -f ./custom-values.yaml multi-juicer multi-juicer/multi-juicer
 
 # We got a example loadbalancer yaml for this example in the repository
 wget -N https://raw.githubusercontent.com/iteratec/multi-juicer/master/guides/k8s/k8s-juice-service.yaml
 
 # Create the loadbalancers
 
-kubectl apply --file k8s-juice-service.yaml
+kubectl apply -f k8s-juice-service.yaml
 # # kubectl apply --namespace monitoring --file k8s-monitoring-service.yaml
 
