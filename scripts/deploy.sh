f0b5cc46a51f58b3cbab526abbd9c5a5ece5daaa
Michael Bonetti <michael.bonetti@incyclesoftware.com>
2022-04-19 10:05:12 -0400

    restore missing logo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
 cat ./.secrets/ghcr-pat-write | docker login ghcr.io -u mbonetti-incyclesoftware --password-stdin
 
-docker build . -t ghcr.io/incyclesoftware/mb-juice-balancer:latest   
-docker push ghcr.io/incyclesoftware/mb-juice-balancer:latest
+headSha=$(git rev-parse HEAD)
+
+docker build ../juice-balancer/ \
+  -t ghcr.io/incyclesoftware/mb-juice-balancer:latest \
+  -t ghcr.io/incyclesoftware/mb-juice-balancer:$headSha
+
+docker push --all-tags ghcr.io/incyclesoftware/mb-juice-balancer
